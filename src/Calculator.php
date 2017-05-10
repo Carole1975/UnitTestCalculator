b873dac5733036183d6fdd7e91101745db8d088a
Carole <carole.velarde@orange.fr>
2017-05-10 12:08:22 +0200

    exo ok

---
+++ src/Calculator.php
@@ -0,0 +1,30 @@
+<?php 
+
+declare(strict_types=1);
+
+final class Calculator{
+
+public function calculate($nb)
+{
+	if($nb==0){
+		return "0";
+
+	}
+
+	if($nb=="1"){
+		return "1";
+	}
+
+	if($nb=="1+2"){
+		return "3";
+	}
+
+
+
+
+
+
+}
+}
+
+?>
